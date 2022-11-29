a7f18ab4d65693e6fd0568eaef9b548a21da4bf4
21Z <69188068+21Z@users.noreply.github.com>
2022-11-29 10:56:28 +0400

    Update invite.js

--- commands/General/invite.js
+++ commands/General/invite.js
@@ -1,29 +1,51 @@
 const Command = require('../../Base/Command.js');
-const { MessageEmbed } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
-class Invite extends Command {
+class Ping extends Command {
 
     constructor(client) {
         super(client);
 
         this.config({
-            name: 'invite',
-            aliases: ['inv'],
-            description: 'Bot invite link',
+            name: 'ping',
+            aliases: ['pong'],
+            description: 'Bot ping',
         });
     }
 
     async run(message) {
-        const embed = new MessageEmbed()
-            .setTitle('Bot Invite')
-            .setDescription(`**[Click Here](https://discord.com/api/oauth2/authorize?client_id=${this.client.user.id}&permissions=2285169728&scope=bot%20applications.commands)** to invite me`)
-            .setColor(0x4d5e94)
-            .setFooter({ text: `Requested by: ${message.author.tag}`, iconURL: message.author.displayAvatarURL() })
-            .setTimestamp();
+        const before = Date.now();
+        const msg = await message.reply({ content: '🏓' });
+        const latency = Date.now() - before;
+        const wsLatency = this.client.ws.ping.toFixed(0);
 
-        message.reply({ embeds: [embed] });
+        const embed = new EmbedBuilder()
+            .setAuthor({
+                name: '🏓 PONG',
+                iconURL: message.author.displayAvatarURL(),
+            })
+            .addFields(
+                {
+                    name: '📶 **|** API',
+                    value: `**\`${latency}\`** ms`,
+                    inline: true,
+                },
+                {
+                    name: '🌐 **|** WebSocket',
+                    value: `**\`${wsLatency}\`** ms`,
+                    inline: true,
+                },
+            )
+            .setFooter({
+                text: `Requested by: ${message.author.tag}`,
+                iconURL: message.author.displayAvatarURL(),
+            })
+            .setTimestamp()
+            .setColor(0x4d5e94);
+
+        msg.edit({ embeds: [embed], content: '\u200b' });
     }
 
 }
 
-module.exports = Invite;
+module.exports = Ping;
