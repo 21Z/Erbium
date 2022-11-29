c5a0a0297748252de2a534ebbca5a8cd4ead281a
21Z <69188068+21Z@users.noreply.github.com>
2022-11-29 10:54:39 +0400

    Update pokemon.js

--- commands/Fun/pokemon.js
+++ commands/Fun/pokemon.js
@@ -1,48 +1,43 @@
 const Command = require('../../Base/Command.js');
 const { EmbedBuilder } = require('discord.js');
-const { Spawn } = require('pokecord');
+const YouTube = require('youtube-sr').default;
 
-class Pokemon extends Command {
+class YouTubeSearch extends Command {
 
     constructor(client) {
         super(client);
 
         this.config({
-            name: 'pokemon',
-            aliases: ['pokemoninfo', 'pokeinfo', 'pokedex', 'pokémon'],
-            description: 'Shows Pokémon info',
+            name: 'youtubesearch',
+            aliases: ['ytsearch', 'yt'],
+            description: 'YouTube Search',
         });
     }
 
     async run(message, args) {
-        const name = args[0];
-        if (!name) return message.reply('❌ | Please include a pokémon name!');
+        const query = args.join(' ');
+        if (!query) return message.reply('❌ | Please include a search query!');
 
         // eslint-disable-next-line no-unused-vars
-        const pokemon = await Spawn(name.toLowerCase()).catch(e => { });
-        if (!pokemon) return message.reply('Oops! Something went wrong...');
-        const height = pokemon.height * 10;
-        const kg = pokemon.weight / 10;
-        const lbs = Math.floor(kg * 2.20462262);
+        const res = await YouTube.searchOne(query, 'video', true).catch(e => {});
+        if (!res) return message.reply('❌ | No result found!');
+
         const embed = new EmbedBuilder()
-            .setTitle('Pokémon Info')
-            .setThumbnail(pokemon.imageURL)
-            .setColor('YELLOW')
+            .setTitle('YouTube Search')
+            .setImage(res.thumbnail.displayThumbnailURL('ultrares'))
             .addFields(
-                { name: 'Name', value: `${pokemon.name}`, inline: true },
-                { name: 'ID', value: `${pokemon.id}`, inline: true },
-                { name: 'Base Experience', value: `${pokemon.baseExperience}`, inline: true },
-                { name: 'Height', value: `${height}cm`, inline: true },
-                { name: 'Weight', value: `${lbs}lbs (${kg}kg)`, inline: true },
-                { name: 'Abilities', value: `${pokemon.abilities.map(m => `\`${m.ability.name}\``).join(', ') || 'None'}` },
-                { name: 'Stats', value: `${pokemon.stats.map(m => `**${m.stat.name}**: \`${m.base_stat}\``).join('\n') || 'None'}` },
+                { name: 'Title', value: `[${res.title}](${res.url})` },
+                { name: 'Author', value: `[${res.channel.name}](${res.channel.url})` },
+                { name: 'Views', value: res.views.toLocaleString() },
+                { name: 'Duration', value: res.durationFormatted },
             )
             .setFooter({ text: `Requested by: ${message.author.tag}`, iconURL: message.author.displayAvatarURL() })
-            .setTimestamp();
+            .setTimestamp()
+            .setColor('RED');
 
-        message.reply({ embeds: [embed] });
+        return message.reply({ embeds: [embed] });
     }
 
 }
 
-module.exports = Pokemon;
+module.exports = YouTubeSearch;
