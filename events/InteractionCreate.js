a5e27866f72359c8738ff9ced18b9f44a3915426
21Z <hamdankalliyil@gmail.com>
2023-10-07 18:06:34 +0400

    Apply dev mode for slash commands

--- events/InteractionCreate.js
+++ events/InteractionCreate.js
@@ -8,13 +8,16 @@ class InteractionCreate extends Event {
     }
 
     async run(interaction) {
+        interaction.user.dev = this.client.config.OWNER.some(x => x === interaction.user.id);
         if (!interaction.isChatInputCommand()) return;
         const commands = this.client.Slashcommands;
         const command = commands.resolve(interaction.options._subcommand) || commands.resolve(interaction.commandName);
 
+        // dev mode
+        if (this.client.config.DEV_MODE && !interaction.user.dev) return interaction.reply({ content: "Commands are restricted to developers only!", ephemeral: true });
+
         if (!interaction.member.permissions.has(command.help.permissions)) return interaction.reply(`❌ | You don't have enough permissions to use this command!\nPermissions Required: ${command.help.permissions.map(m => `\`${m.replace(/([A-Z])/g, " $1").trim()}\``).join(", ")}`);
         if (!interaction.guild.members.me.permissionsIn(interaction.channel).has(command.help.botPerms)) return interaction.reply(`❌ | I do not have enough permissions to use this command!\nPermissions Required: ${command.help.botPerms.map(m => `\`${m.replace(/([A-Z])/g, " $1").trim()}\``).join(", ")}`);
-
         if (!command) return;
 
         try {
