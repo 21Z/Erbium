295e7ab25441138ef037cd9490497b432c69bbec
21Z <hamdankalliyil@gmail.com>
2024-02-24 15:04:35 +0400

    Small Improvements

--- config.example.js
+++ config.example.js
@@ -4,14 +4,14 @@ const config = {
     COMMANDS_DIR: `${__dirname}/commands`, // commands dir
     EVENTS_DIR: `${__dirname}/events`, // events dir
     DEV_MODE: false, // dev mode
-    OWNER: ["1234567890123456"], // Bot owner id
+    OWNER: ["1234567890123456"], // Bot owner(s) Id
     CLIENT_ID: ["1234567890123456"], // Bot id
-    GUILD_ID: ["1234567890123456"], // your server ID for message edit/delete logs
-    CHANNEL_ID: ["1234567890123456"], // channel id for chatgpt chatbot
+    GUILD_ID: ["1234567890123456"], // Your server Id for message edit/delete logs
+    CHANNEL_ID: ["1234567890123456"], // Channel Id for chatgpt chatbot
     PREFIX: "er!", // command prefix
-    EMBED_COLOR: 0x4d5e94, // embed color
-    SuccessEmoji: "✅", // emoji to use when sending success messages
-    ErrorEmoji: "❌", // emoji to use when sending error messages
+    EMBED_COLOR: 0x4d5e94, // Embed color
+    SuccessEmoji: "✅", // Emoji to use when sending success messages
+    ErrorEmoji: "❌", // Emoji to use when sending error messages
 };
 
 module.exports = config;
