0f6fe4a3ecde8896ae97e31d036d6ce7c2a7c589
21Z <hamdankalliyil@gmail.com>
2024-03-28 09:34:51 +0400

    Fix mistake in ownerOnly check

--- events/InteractionCreate.js
+++ events/InteractionCreate.js
@@ -15,7 +15,7 @@ class InteractionCreate extends Event {
 
         // dev mode
         if (this.client.config.DEV_MODE && !interaction.user.dev) return interaction.reply({ content: "Commands are restricted to developers only!", ephemeral: true });
-        if (command.ownerOnly && !interaction.user.dev) return;
+        if (command.help.ownerOnly && !interaction.user.dev) return;
 
         if (!interaction.member.permissions.has(command.help.permissions)) return interaction.reply(`❌ | You don't have enough permissions to use this command!\nPermissions Required: ${command.help.permissions.map(m => `\`${m.replace(/([A-Z])/g, " $1").trim()}\``).join(", ")}`);
         if (!interaction.guild.members.me.permissionsIn(interaction.channel).has(command.help.botPerms)) return interaction.reply(`❌ | I do not have enough permissions to use this command!\nPermissions Required: ${command.help.botPerms.map(m => `\`${m.replace(/([A-Z])/g, " $1").trim()}\``).join(", ")}`);
