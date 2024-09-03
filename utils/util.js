9aaba66f0b42edeb4f1040eb397c3005c50e2083
21Z <hamdankalliyil@gmail.com>
2024-09-03 22:10:58 +0400

    Remove unnecessary expression

--- utils/util.js
+++ utils/util.js
@@ -17,7 +17,7 @@ class Util {
         if (typeof text !== "string") text = require("util").inspect(text, { depth: 2 });
 
         text = text
-            .replace(new RegExp(token, "g") ?? "", "mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0")
+            .replace(new RegExp(token, "g"), "mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0")
             .replace(/`/g, "`" + String.fromCharCode(8203))
             .replace(/@/, "@" + String.fromCharCode(8203));
 
